e068d724a96e5e9b212ba00fdc29e6c11a0db006
graham-hughes-code <grahamhughes@optonline.net>
2023-03-23 21:57:08 -0400

    add-orientation
    - added orientation of the flowchart to the cli
    - added small amount of error handling for cli

--- pyflow/pyflow_cli.py
+++ pyflow/pyflow_cli.py
@@ -4,13 +4,33 @@ import argparse
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('path', nargs='*')
+  parser.add_argument('path', nargs='*', help='The path of the file to pull flowchart from.')
+  parser.add_argument('-o',
+                      '--orientation',
+                      default='TB',
+                      help='The orientation of the flowchart can '
+                           'be Possible FlowChart orientations are: '
+                           'TB - top to bottom. '
+                           'TD - top-down same as top to bottom. '
+                           'BT - bottom to top. '
+                           'RL - right to left. '
+                           'LR - left to right.')
   args = parser.parse_args()
 
-  func_name_to_docstring_map = pull_doc_string_from_file(args.path[0])
+  if not args.path:
+    print('Error: file not given.')
+    return 1
+
+  path = args.path[0]
+
+  if (orientation := args.orientation.upper()) not in ['TB', 'TD', 'BT', 'RL', 'LR']:
+    print(f'Error: {args.orientation} is not a valid orientation.')
+    return 1
+
+  func_name_to_docstring_map = pull_doc_string_from_file(path)
 
   mermaid_map = create_mermaid_map(func_name_to_docstring_map)
 
-  print(build_full_chart(mermaid_map=mermaid_map, orientation='TD'))
+  print(build_full_chart(mermaid_map=mermaid_map, orientation=orientation))
 
   return 0
